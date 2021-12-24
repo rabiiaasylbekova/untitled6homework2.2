8e05b0786fa097c79708cdeaca873650b050d5d6
rabiiaasylbekova <rabiaasylbekova1@gmail.com>
2021-12-24 16:44:36 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(num(67, 20));
+    }
+
+
+    public static String num(int ageOfMe, int temperature) {
+
+        if (ageOfMe <= 20 || ageOfMe >= 45 || temperature <= -20 || temperature <= 45) {
+            if (ageOfMe < 20 && temperature > 0 && temperature < 20) {
+                return "Можно идти гулять";
+            } else if (ageOfMe > 45 && temperature > -10 && temperature < 25) {
+
+                return "Можно идти гулять";
+
+            } else
+                return "Оставайтесь дома";
+        } else {
+            return "Можно идти гулять";
+        }
+
+
+    }
+
+
+}
+
+
+
+
